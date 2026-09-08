317075d8673e9ae92550e02946469e8b99d9b5b4
atidepod <cs260aws@gmail.com>
2026-09-08 15:46:40 -0600

    Auto-checkpoint: 2026-09-08 15:46:40

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -29,14 +29,15 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return TeamColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+
+        return PeiceType;
     }
 
     /**
