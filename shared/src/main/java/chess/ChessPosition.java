7898ce7414ea3ce9b7cf6cf58d84894c976bde07
atidepod <cs260aws@gmail.com>
2026-09-14 19:24:22 -0600

    Auto-checkpoint: 2026-09-14 19:24:22

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -16,7 +16,7 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return 
     }
 
     /**
