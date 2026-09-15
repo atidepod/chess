863aec8d3c54c9862f1986cbdee8442cce739d66
atidepod <cs260aws@gmail.com>
2026-09-14 19:04:15 -0600

    Auto-checkpoint: 2026-09-14 19:04:15

--- shared/src/main/java/chess/HelloWorld.java
+++ shared/src/main/java/chess/HelloWorld.java
@@ -5,8 +5,9 @@ import java.util.Arrays;
 public class HelloWorld {
     public static void main(String[] args){
         var gs = new GetSetExample();
-        gs.scores[2] = 50;
-        System.out.println(Arrays.toString(gs.scores));
+        gs.getScores()[2] = 50;
+        System.out.println(Arrays.toString(gs.getScores()));
+        gs.setScores(new int)
     }
     public int[] scores = new int[10];
 }
